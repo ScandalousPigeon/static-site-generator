4d9ec4a64146f81e67308cd45ba91b4b9eb908c0
ScandalousPigeon <951413eelnoel@gmail.com>
2024-10-06 22:11:47 +1100

    implemented textnode class, tested __repr__ with a dummy object in the main file

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+def main():
+    dummy = TextNode("The Infinite Pigeon Theorem", "bold", "https://xmxx.com")
+    print(dummy)
+
+if __name__ == "__main__":
+    main()
+
+
